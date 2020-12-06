deefbef4b32fd44e95ea007a917d09641a832055
majian <565507114@qq.com>
2020-12-06 22:42:13 +0800

    切面类修改

--- SpringAopDemo/src/main/java/org/example/demoAspect/BrokerAspect.java
+++ SpringAopDemo/src/main/java/org/example/demoAspect/BrokerAspect.java
@@ -21,51 +21,51 @@ public class BrokerAspect {
 
     }
 
-//    /**
-//    * @description  在连接点执行之前执行的通知
-//    */
-//    @Before("BrokerAspect()")
-//    public void doBeforeGame(){
-//        System.out.println("经纪人正在处理球星赛前事务！");
-//    }
-//
-//    /**
-//     * @description  在连接点执行之后执行的通知（返回通知和异常通知的异常）
-//     */
-//    @After("BrokerAspect()")
-//    public void doAfterGame(){
-//        System.out.println("经纪人为球星表现疯狂鼓掌！");
-//    }
-//
-//    /**
-//     * @description  在连接点执行之后执行的通知（返回通知）
-//     */
-//    @AfterReturning("BrokerAspect()")
-//    public void doAfterReturningGame(){
-//        System.out.println("返回通知：经纪人为球星表现疯狂鼓掌！");
-//    }
-//
-//    /**
-//     * @description  在连接点执行之后执行的通知（异常通知）
-//     */
-//    @AfterThrowing("BrokerAspect()")
-//    public void doAfterThrowingGame(){
-//        System.out.println("异常通知：球迷要求退票！");
-//    }
+    /**
+    * @description  在连接点执行之前执行的通知
+    */
+    @Before("BrokerAspect()")
+    public void doBeforeGame(){
+        System.out.println("经纪人正在处理球星赛前事务！");
+    }
 
     /**
-     * @description  使用环绕通知
-     * 由于该函数的使用，前端页面无法得到正确的值
+     * @description  在连接点执行之后执行的通知（返回通知和异常通知的异常）
      */
-    @Around("BrokerAspect()")
-    public void doAroundGame(ProceedingJoinPoint pjp) throws Throwable {
-        try{
-            System.out.println("经纪人正在处理球星赛前事务！");
-            pjp.proceed();
-            System.out.println("返回通知：经纪人为球星表现疯狂鼓掌！");
-        }
-        catch(Throwable e){
-            System.out.println("异常通知：球星因为某种原因未能出战，球迷要求退票！");
-        }
+    @After("BrokerAspect()")
+    public void doAfterGame(){
+        System.out.println("经纪人为球星表现疯狂鼓掌！");
     }
+
+    /**
+     * @description  在连接点执行之后执行的通知（返回通知）
+     */
+    @AfterReturning("BrokerAspect()")
+    public void doAfterReturningGame(){
+        System.out.println("返回通知：经纪人为球星表现疯狂鼓掌！");
+    }
+
+    /**
+     * @description  在连接点执行之后执行的通知（异常通知）
+     */
+    @AfterThrowing("BrokerAspect()")
+    public void doAfterThrowingGame(){
+        System.out.println("异常通知：球迷要求退票！");
+    }
+
+//    /**
+//     * @description  使用环绕通知
+//     * 由于该函数的使用，前端页面无法得到正确的值
+//     */
+//    @Around("BrokerAspect()")
+//    public void doAroundGame(ProceedingJoinPoint pjp) throws Throwable {
+//        try{
+//            System.out.println("经纪人正在处理球星赛前事务！");
+//            pjp.proceed();
+//            System.out.println("返回通知：经纪人为球星表现疯狂鼓掌！");
+//        }
+//        catch(Throwable e){
+//            System.out.println("异常通知：球星因为某种原因未能出战，球迷要求退票！");
+//        }
+//    }
 }
