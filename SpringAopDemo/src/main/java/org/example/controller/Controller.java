8a1aaa6711417de9ab53a83c8c366977afb4f2ef
majian <565507114@qq.com>
2020-12-06 20:03:20 +0800

    init project

---
+++ SpringAopDemo/src/main/java/org/example/controller/Controller.java
@@ -0,0 +1,49 @@
+package org.example.controller;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Deacription TODO
+ * @Author majian
+ * @Date 2020/12/6
+ * @Version 1.0
+ **/
+
+@RestController
+@RequestMapping("/aopController")
+public class Controller {
+
+
+    @RequestMapping(value = "/Curry")
+    public String Curry(){
+        System.out.println("库里上场打球了！！");
+        return "库里上场打球了！！";
+    }
+
+    @RequestMapping(value = "/Harden")
+    public String Harden(){
+        System.out.println("哈登上场打球了！！");
+        return "哈登上场打球了！！";
+    }
+
+    @RequestMapping(value = "/Antetokounmpo")
+    public String  Antetokounmpo(){
+        System.out.println("字母哥上场打球了！！");
+        return "字母哥上场打球了！！";
+    }
+
+    @RequestMapping(value = "/Jokic")
+    public String Jokic(){
+        System.out.println("约基奇上场打球了！！");
+        return "约基奇上场打球了！！";
+    }
+
+    @RequestMapping(value = "/Durant/{point}")
+    public String Durant(@PathVariable("point")  int point){
+        System.out.println("杜兰特上场打球了！！");
+        return "杜兰特上场打球了！！";
+    }
+
+}
