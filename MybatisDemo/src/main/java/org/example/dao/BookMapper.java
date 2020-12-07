bed89f0615adcaf049129a4295a4fd79d8513387
majian <565507114@qq.com>
2020-12-07 18:13:35 +0800

    add module MybatisDemo

---
+++ MybatisDemo/src/main/java/org/example/dao/BookMapper.java
@@ -0,0 +1,30 @@
+package org.example.dao;
+
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+import org.example.entity.Book;
+import org.example.entity.BookExample;
+
+public interface BookMapper {
+    long countByExample(BookExample example);
+
+    int deleteByExample(BookExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(Book record);
+
+    int insertSelective(Book record);
+
+    List<Book> selectByExample(BookExample example);
+
+    Book selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") Book record, @Param("example") BookExample example);
+
+    int updateByExample(@Param("record") Book record, @Param("example") BookExample example);
+
+    int updateByPrimaryKeySelective(Book record);
+
+    int updateByPrimaryKey(Book record);
+}
